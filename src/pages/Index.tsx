a0fd09fc6b8984d5f7639af21742c453bc22d677
nyeonseok <123443488+nyeonseok@users.noreply.github.com>
2025-10-22 20:27:07 +0900

    feat: landing아이콘 삽입

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,3 +1,14 @@
+import indexStyle from "../css/pages/landingStyle.module.css";
+import landingLogo from "../assets/landingLogo.svg";
+
 export default function Index() {
-  return <div>Index Page</div>;
+  return (
+    <div className={indexStyle.container}>
+      <img
+        src={landingLogo}
+        alt="landingLogo"
+        className={indexStyle.landingLogo}
+      />
+    </div>
+  );
 }
